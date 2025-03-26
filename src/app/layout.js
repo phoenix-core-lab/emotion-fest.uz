7c06991861f61b70add6b8c17495e91e70067c17
Timur Kasimov <106226856+timakasimovktr@users.noreply.github.com>
2025-03-26 14:28:35 +0500

    Partners

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Montserrat } from "next/font/google";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Script from "next/script"; // Импортируем next/script
 import "./globals.sass";
-
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -15,11 +15,39 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-       <link rel="icon" href="/logoico.svg" />
+        <link rel="icon" href="/logoico.svg" />
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(100373346, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true,
+                  ecommerce:"dataLayer"
+              });
+            `,
+          }}
+        />
       </head>
       <body className={montserrat.className}>
-        <Header/>{children}<Footer/>
-        </body>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/100373346" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
-} 
+}
--- src/components/intro/partners/index.jsx
+++ src/components/intro/partners/index.jsx
@@ -4,63 +4,41 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 // import "./styles.sass";
 import "swiper/css";
+import Image from "next/image";
 
 const PartnersSlider = () => {
   const PartnersSlideImages = [
-    { id: 1, img: "/base.png", alt: "base" },
-    { id: 2, img: "/phoenix.png", alt: "after", title: "Phoenix Core Lab." },
-    { id: 3, img: "/maze.png", alt: "maze" },
-    { id: 4, img: "/base.png", alt: "base" },
-    { id: 5, img: "/phoenix.png", alt: "after", title: "Phoenix Core Lab." },
-    { id: 6, img: "/maze.png", alt: "maze" },
+    { id: 1, img: "/imas.png", alt: "after", title: "iMAS GROUP — международная IT компания в области мониторинга СМИ и социальных сетей. На протяжении 11 лет мы предоставляем услуги по сбору, анализу и интерпретации медиа-данных, помогая нашим клиентам принимать стратегически важные решения. Наша команда объединяет передовые технологии и экспертный подход, чтобы предложить качественную аналитику и поддержку в управлении репутацией. Среди клиентов — крупные бренды и государственные структуры, которые доверяют нам обработку информации и создание медиа-отчетов. Мы стремимся стать надежным партнером для всех, кто ценит точные данные и инновационные решения в мире медиа. https://imas.uz/" },
+    { id: 2, img: "/alem.svg", alt: "after", title: "Информационный партнёр Всё самое интересное из мира IT-индустрии и технологий https://aleminform.kz/" },
   ];
   return (
     <div>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={6}
+        slidesPerView={1}
         loop={true}
         autoplay={{ delay: 0 }}
         freeMode={true}
         modules={[Autoplay]}
-        speed={3000}
-        breakpoints={{
-          0: {
-            slidesPerView: 2,
-          },
-          420: {
-            slidesPerView: 3,
-          },
-          577: {
-            slidesPerView: 3,
-          },
-          911: {
-            slidesPerView: 3,
-          },
-          1100: {
-            slidesPerView: 3,
-          },
-        }}
+        speed={6000}
       >
         {PartnersSlideImages.map((item, index) => (
           <SwiperSlide key={index}>
             <div className="imagesBlock">
-              {/* <img
+              <Image
                 src={item.img}
                 alt={item.alt}
-                className='img'
-              /> */}
+                width={100} 
+                height={100} 
+              />
               <p className="text">
                 {
-                // item.title ? item.title : ""  
+                  item.title ? item.title : ""
                 }
-                Ваш логотип
               </p>
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
-      
     </div>
   );
 };
